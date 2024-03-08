e00555d6da0f8d1ec64619474fcf23df0199086a
seif namissi <seifeddine.namissi@medtech.tn>
2024-03-08 11:10:36 +0100

    seif's first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Reportfunction from './components/Reportfunction';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+   
+   
+   <View style={styles.container}>
       <Text>Vroom App!</Text>
       <StatusBar style="auto" />
+      <Reportfunction/>
     </View>
   );
 }
