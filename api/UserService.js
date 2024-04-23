85df34f5f047794f98e0b5b12bd6a993be6c74df
Karim Saadaoui <karim.saadaoui@medtech.tn>
2024-04-24 00:23:28 +0100

    fixed the image upload and linked with backend

--- api/UserService.js
+++ api/UserService.js
@@ -1,4 +1,5 @@
 require("../config/Firebase");
+import * as FileSystem from 'expo-file-system';
 
 import {
   getAuth,
@@ -10,8 +11,10 @@ import {
 import apiConfig from "./apiConfig";
 import axios from "axios";
 const auth = getAuth();
+
 const createUser = async (userValidatedPayload) => {
   const url = `${apiConfig.baseURL}/user/signup`;
+  console.log("URL" , url);
   let FbaseUser;
   try {
     FbaseUser = await createUserWithEmailAndPassword(
@@ -19,13 +22,18 @@ const createUser = async (userValidatedPayload) => {
       userValidatedPayload.email,
       userValidatedPayload.password
     );
+
     await sendEmailVerification(FbaseUser.user);
+
     const response = await axios.post(url, {
       ...userValidatedPayload,
       firebaseId: FbaseUser.user.uid,
     });
+
     console.log("SUCCESS: User created successfully");
+
     return response;
+    
   } catch (err) {
     console.error("Error creating user:", err);
     // Handle Firebase errors
@@ -66,4 +74,43 @@ const signIn = async (email, password) => {
   }
 };
 
-module.exports = { signIn, createUser, setPreferences };
+const saveImage = async (profileImage) => {
+  const imgDir = FileSystem.documentDirectory;
+
+  if (!profileImage) {
+      console.error('No image selected.');
+      return null;
+  }
+
+  const filename = new Date().getTime() + '.jpg';
+
+  try {
+    const url = `${apiConfig.baseURL}/user/upload-image`; 
+
+  const formData = new FormData();
+  formData.append('image', {
+      uri: profileImage,
+      name: filename,
+      type: 'image/jpg',
+  });
+
+  const response = await axios.post(url, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+      });
+
+      const data = response.data;
+
+      if (data.success) {
+          console.log('Image uploaded successfully:', data.message);
+          return data.imagePath;
+      } else {
+          console.error('Image upload failed:', data.error);
+      }
+  } catch (error) {
+      console.error('Error uploading image:', error);
+  }
+};
+
+module.exports = { signIn, createUser, setPreferences , saveImage };
